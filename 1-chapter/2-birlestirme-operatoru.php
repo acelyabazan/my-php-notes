355135acf0e6806191162e5cc76cc16c7fc8b992
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-05-13 10:05:35 +0300

    Add files via upload

---
+++ 1-chapter/2-birlestirme-operatoru.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birleştimre Operatörü</title>
+</head>
+<body>
+    <?php
+        // Birleştirme operatörü: "."
+        // Birden fazla echo etiketi kullanmak yerine birleştirme operatörümüzü kullanıyoruz.
+        echo "Bugün Udemy dersimizde birleştirme operatörünü öğreniyoruz."." ~Açelya"." ~Rıdvan";
+    ?>
+</body>
+</html>

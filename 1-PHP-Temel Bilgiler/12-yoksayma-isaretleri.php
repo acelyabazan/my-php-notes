522a0cb935c9f36e338dbf3480570d0643a852e8
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-05-20 20:06:26 +0300

    Add files via upload

---
+++ 1-PHP-Temel Bilgiler/12-yoksayma-isaretleri.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yoksayma İşaretleri</title>
+</head>
+<body>
+    <?php
+        // Yoksayma İşaretleri:
+
+        // Aşağıdaki yapacağımız örnekte Udemy kısmına çift tırnak koymak istiyorum("Udemy"). Fakat çift tırnağın içerisine çift tırnak koyamayacağımız için yoksayma işareti (\) kullanacağız:
+        echo "Ben \"Udemy\" kursuna kayıt oldum";
+    ?>
+</body>
+</html>

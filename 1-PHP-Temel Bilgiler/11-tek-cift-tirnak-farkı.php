5851355d414e77b73956cdbeec6c9a694f97b31e
Açelya Bazan <111985667+acelyabazan@users.noreply.github.com>
2023-05-20 19:53:40 +0300

    Add files via upload

---
+++ 1-PHP-Temel Bilgiler/11-tek-cift-tirnak-farkı.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tek ve Çift Tırnak Arasındaki Fark</title>
+</head>
+<body>
+    <?php
+
+        // " Çift tırnak ve ' tek tırnak arasındaki farklar:
+        
+        $Ad = "Açelya";
+        $Soyad = "Bazan";
+
+        // Çift tırnak içerisinde değişken içerikleri okunurken, tek tırnak içerisindeki değişken içerikleri okunmaz:
+        echo "Benim adım $Ad";
+        echo "<br>";
+        echo 'Benim adım $Ad';
+    ?>
+</body>
+</html>
